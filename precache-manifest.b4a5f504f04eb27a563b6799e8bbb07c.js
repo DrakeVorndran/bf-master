00d4fbd80eafbddc55de6f1c19dea6e8675d3e57
Ki Vorndran <drake.vorndran@students.makeschool.com>
2020-01-09 21:14:48 -0800

    Updates

---
+++ precache-manifest.b4a5f504f04eb27a563b6799e8bbb07c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e32b36aa9237a41e8a4f673824a9424",
+    "url": "/bf-master/index.html"
+  },
+  {
+    "revision": "3ab42dfe2ceb62ef9158",
+    "url": "/bf-master/static/css/main.81d37f69.chunk.css"
+  },
+  {
+    "revision": "04ff0c6db24481d93ca5",
+    "url": "/bf-master/static/js/2.f0c31c10.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/bf-master/static/js/2.f0c31c10.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ab42dfe2ceb62ef9158",
+    "url": "/bf-master/static/js/main.c219a5c7.chunk.js"
+  },
+  {
+    "revision": "66204c0d20d41969b1f8",
+    "url": "/bf-master/static/js/runtime-main.9a2d3548.js"
+  }
+]);
